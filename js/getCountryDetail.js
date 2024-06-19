5a5c83b3000c059403e31a926496873296c9b009
k1ssa1 <samikissai.edu@gmail.com>
2024-06-19 22:15:44 +0200

    fixing country list names

--- js/getCountryDetail.js
+++ js/getCountryDetail.js
@@ -18,15 +18,27 @@ let secondList = [tld, currencies, languages]
 function createCountryCard(){
     document.getElementById("countryCardImg").appendChild(flagIcon);
     document.getElementById("countryCardTitle").innerHTML = countryName
+    let i = 0;
+    let j = 0;
     firstList.map(element =>{
+        let listName = ["Native name :","Population :","Region :","Sub Region :", "Capital: "]
         let listItem = document.createElement("li");
-        listItem.innerHTML = element
-        document.getElementById("firstList").appendChild(listItem)
+        let listNameComponent = document.createElement("span");
+        listNameComponent.innerHTML = listName[i];
+        listNameComponent.className = "listNames";
+        listItem.innerHTML = listNameComponent.innerHTML + " " + element;
+        document.getElementById("firstList").appendChild(listItem);
+        i = i + 1;
     })
     secondList.map(element =>{
+        let listName2 = ["Top level domain : ","Currencies : ","Languages : "]
         let listItem = document.createElement("li");
-        listItem.innerHTML = element
-        document.getElementById("secondList").appendChild(listItem)
+        let listNameComponent2 = document.createElement("span");
+        listNameComponent2.innerHTML = listName2[j];
+        listNameComponent2.className = "listNames";
+        listItem.innerHTML = listNameComponent2.innerHTML + " " + element;
+        document.getElementById("secondList").appendChild(listItem);
+        j = j + 1
     })
 
     dispayBorders();
